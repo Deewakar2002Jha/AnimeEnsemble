5e6eadbf67e1618e2f87df430b465f6ed6bf530e
Deewakar2002Jha <deewakarjha0541@gmail.com>
2024-01-18 03:27:38 +0530

    MY first MERN STACK PROJECT

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React ,{ useState } from 'react';
+import './Navbar.css'
+import logo from '../Assets/websitelogo2.png'
+import cart_icon from '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom';
+export const Navbar = () => {
+
+  const[menu,setMenu] = useState("shop");
+
+  return (
+    <div className='navbar'>
+        <div className="nav-logo">
+            <img src={logo} alt="" className="main-logo"/>
+            <p>AnimeEnsemble</p>
+        </div>
+        <ul className="nav-menu">
+            <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration:'none'}} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("mens")}}><Link style={{textDecoration:'none'}} to='/mens'>Mens</Link>{menu==="mens"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("womens")}}><Link style={{textDecoration:'none'}} to='/womens'>Womens</Link>{menu==="womens"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("kids")}}><Link style={{textDecoration:'none'}} to='/kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
+        </ul>
+        <div className="nav-login-cart">
+            <Link to='/Login'><button>Login</button></Link>
+            <Link to='/cart'><img src={cart_icon} alt="" /></Link>
+            <div className="nav-cart-count">0</div>
+        </div>
+    </div>
+  )
+}
---
+++ src/Pages/Product.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ShopCategory } from './ShopCategory'
+
+export const Product = () => {
+  return (
+    <div>Product</div>
+  )
+}
+
+export default Product
